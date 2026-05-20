0b920a5397aafeb784a33a8df8be8e02dfdd942e
Orjales Garcia <472760@collierschools.com>
2026-05-20 12:22:38 -0400

    Update new website project style

--- final-project/script.js
+++ final-project/script.js
@@ -1,38 +1,14 @@
-// Javascript Hell starts here DO NOT JUDGE
+const textElement = document.getElementById('type-text');
+const textToType = "I hope this message finds you well.";
+let index = 0;
 
-// Video Assets
-var bootUpVideo = document.getElementById("bootUpVideo");
+function typeEffect() {
+    if (index < textToType.length) {
+        textElement.textContent += textToType.charAt(index);
+        index++;
+        setTimeout(typeEffect, 50); 
+    }
+}
 
-// Audio Assets
-var bootUpAudio = new Audio("assets/audio/boot-up.mp3");
-var computerFans = new Audio("assets/audio/computer-fans.mp3");
-var error = new Audio("assets/audio/error.mp3");
-var floppyDisk = new Audio("assets/audio/floppy-disk-insert.mp3");
-
-console.log('Script Loaded')
-
-// Boot Up Sequence
-
-// var skipSequence = false;
-
-function playBootUpSequence() {
-  if (skipSequence === false) {
-    console.log("Playing boot up sequence...");
-    floppyDisk.play();
-    setTimeout(() => {
-      computerFans.play();
-      bootUpVideo.style.display = 'flex'
-      bootUpVideo.play();
-    }, 2000);
-
-    bootUpVideo.addEventListener('ended', (event) => {
-      bootUpVideo.style.display = 'none'
-      bootUpAudio.play();
-    });
-
-  }
-  else {
-    console.log("Boot Up Skipped")
-}}
-
-playBootUpSequence();
+// Start the effect
+typeEffect();
